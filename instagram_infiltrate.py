577afc002660fb2042dcc632d45aca3d8d4168bc
Kendrik Wah <48517661+kendrik-wah@users.noreply.github.com>
2019-09-22 20:17:50 +0800

    Add files via upload

---
+++ instagram_infiltrate.py
@@ -0,0 +1,25 @@
+from InstagramAPI import InstagramAPI
+
+user_name = 'kendrikwah'
+password = 'xxxxxxx'
+
+acc = InstagramAPI(username=user_name, password=password)
+
+acc.login()
+
+acc.getProfileData()
+profile_data = acc.LastJson
+print(profile_data)
+
+acc.getSelfUserFeed()
+user_feed = acc.LastJson
+print(user_feed)
+
+acc.getRecentActivity()
+recent_activity = acc.LastJson
+print(recent_activity)
+
+acc.getSelfSavedMedia()
+saved_media = acc.LastJson
+print(saved_media)
+
