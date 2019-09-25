713e2d05c2f408dc9e39be237d514233badcf4b1
Kendrik Wah <48517661+kendrik-wah@users.noreply.github.com>
2019-09-25 19:08:23 +0100

    Add files via upload

---
+++ scrape_script.py
@@ -0,0 +1,14 @@
+from instagram_infiltrate import InstagramScrape
+from twitter_infiltrate import TwitterTarget
+from reddit_scrape import RedditTarget
+
+# need to take relevant values off a person's account
+# instagram_handle = InstagramScrape('xxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxx')
+# twitter_handle = TwitterTarget('ChaotiqueEdge')
+reddit_handle = RedditTarget()
+
+mental_illnesses = ['depression', 'anxiety', 'stress']
+
+# instagram_handle.scrape(mental_illnesses)
+# twitter_handle.scrape(mental_illnesses)
+# reddit_handle.scrape(mental_illnesses)
